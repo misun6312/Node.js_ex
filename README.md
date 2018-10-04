ad509f9b6955d5d5924e066ffca8cdda27c9adc2
Alice M. Yoon <misun6312@gmail.com>
2018-10-04 19:24:19 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Node.js_ex
+# Node.js Exercise
+
+## Installation
+
+After clone this repository, to install all the node_modules from the package.json file you simply put: 
+```
+$npm install 
+```
+in terminal (on the same directory where the package.json exists) and it would install all the node modules in the folder called node_modules.
+
+## Usage
+
+After you cloned the repository, you can run the web server using :
+```
+$ node app.js
+```
+then, visit [http://localhost:3000][address].
+
+## Testing
+
+
+
+[address]:http://localhost:3000
--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 var express = require('express');
 var bodyParser = require('body-parser')
-var stylus = require('stylus')
-var nib = require('nib')
 var app = express();
 
 app.locals.pretty = true;
@@ -25,18 +8,6 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
-function compile(str, path) {
-    return stylus(str)
-        .set('filename', path)
-        .use(nib())
-}
-
-app.use(stylus.middleware({
-    src: __dirname + '/public',
-    compile: compile
-}))
 
 // database
 var mysql      = require('mysql');
